128bdd0ddac9954181dac6d8570ee11b7833db47
Adam Ziff <adamhziff@gmail.com>
2022-12-27 16:27:06 -0500

    set up again

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn backend:app --python-version 3.9
---
+++ backend.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+# CORS(app, origins=['http://localhost:3000'])
+CORS(app)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({ 'data': 'Hello, World!' })
